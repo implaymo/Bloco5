5331ace408e3717381067fa5769a74b77aebc2f1
implaymo <goncalomoreirasilva@gmail.com>
2024-11-08 10:20:44 +0000

    Finish 2_4

---
+++ src/main/java/org/example/Exercicio2/Exercicio2_4.java
@@ -0,0 +1,24 @@
+package org.example.Exercicio2;
+
+public class Exercicio2_4 {
+
+    public static int exercicio2_4(int[][] array){
+        if (Exercicio2_2.isArrayEmpty(array)){
+            return -1;
+        }
+
+        int totalProduct = 1;
+        for (int i = 0; i < array.length; i++) {
+            totalProduct *= multiplicateLineNumbers(array[i]);
+        }
+        return  totalProduct;
+    }
+
+    public static int multiplicateLineNumbers(int[] array) {
+        int mult = 1;
+        for (int i = 0; i < array.length; i++) {
+            mult *= array[i];
+        }
+        return mult;
+    }
+}
---
+++ src/test/java/org/example/Exercicio2/Exercicio2_4Test.java
@@ -0,0 +1,41 @@
+package org.example.Exercicio2;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Exercicio2_4Test {
+
+    @Test
+    void test_constructor() {
+        new Exercicio2_4();
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider")
+    void should_return_correct_answers(int[][] array, double expected) {
+        //arrange
+        //act
+        int result = Exercicio2_4.exercicio2_4(array);
+        //assert
+        assertEquals(expected, result);
+    }
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider() {
+        return Stream.of(
+                Arguments.of(new int[][]{{1, 1},{2}, {3, 3}}, 18),
+                Arguments.of(new int[][]{{1, 3},{2, 5}, {6, 7}}, 1260),
+                Arguments.of(new int[][]{{} ,{}, {}}, -1),
+                Arguments.arguments(new int[][]{{1, 1},{}, {3}}, -1),
+                Arguments.arguments(new int[][]{{1, },{2, }, {3}}, 6),
+                Arguments.arguments(new int[][]{{1, 2},{2, 3}, {}}, -1)
+        );
+    }
+}
