6f0eec204e613306914e5505fc4b117ea0d40c98
implaymo <goncalomoreirasilva@gmail.com>
2024-11-12 22:47:22 +0000

    Finish exercicio4_3

--- src/main/java/org/example/Exercicio4/Exercicio4_3.java
+++ src/main/java/org/example/Exercicio4/Exercicio4_3.java
@@ -4,33 +4,65 @@ import java.util.ArrayList;
 
 public class Exercicio4_3 {
 
-    public static boolean findWordProvided(char[][] boardGame, ArrayList<Character> wordToFind, int rowBeginWord,
-                                           int columnBeginWord, int rowEndWord, int columnEndWord) {
+    public static boolean findWordProvided(char[][] boardGame, ArrayList<Character> wordToFind, int beginRow,
+                                           int beginColumn, int endRow, int endColumn) {
 
 
 
-        if (rowBeginWord == rowEndWord && columnBeginWord <= columnEndWord) {
+        if (beginRow == endRow && beginColumn <= endColumn ) {
             // Horizontal Left to Right
+            if(Exercicio4_2.matchHorizontal(boardGame, wordToFind, beginRow, beginColumn)){
+                return true;
+            }
         }
 
-        if(rowBeginWord == rowEndWord && columnBeginWord >= columnEndWord){
+        if(beginRow == endRow && beginColumn >= endColumn){
             // Horizontal Right to Left
+            if(Exercicio4_2.matchHorizontalReversed(boardGame, wordToFind, beginRow, beginColumn)){
+                return true;
+            }
         }
 
-        if(columnBeginWord == columnEndWord && rowBeginWord <= rowEndWord) {
+        if(beginColumn == endColumn && beginRow <= endRow) {
             // Vertical Top to Bottom
+            if(Exercicio4_2.matchVertical(boardGame, wordToFind, beginRow, beginColumn)){
+                return true;
+            }
         }
 
-        if(columnBeginWord == columnEndWord && rowBeginWord >= rowEndWord) {
+        if(beginColumn == endColumn && beginRow >= endRow) {
             // Vertical Bottom to Top
+            if(Exercicio4_2.matchVerticalReversed(boardGame, wordToFind, beginRow, beginColumn)){
+                return true;
+            }
         }
 
-        if(rowBeginWord == columnBeginWord && rowEndWord == columnEndWord && rowBeginWord < rowEndWord) {
-            // Diagonal Top Left or Right
+        // Diagonal Top Left to Bottom Right
+        if (beginRow < endRow && beginColumn < endColumn) {
+            if (Exercicio4_2.matchDiagonalTopLeft(boardGame, wordToFind, beginRow, beginColumn)) {
+                return true;
+            }
         }
 
-        if(rowBeginWord == columnBeginWord && rowEndWord == columnEndWord && rowBeginWord > rowEndWord) {
-            // Diagonal Bottom Left or Right
+        // Diagonal Bottom Right to Top Left
+        if (beginRow > endRow && beginColumn > endColumn) {
+            if (Exercicio4_2.matchDiagonalBottomRight(boardGame, wordToFind, beginRow, beginColumn)) {
+                return true;
+            }
+        }
+
+        // Diagonal Top Right to Bottom Left
+        if (beginRow < endRow && beginColumn > endColumn) {
+            if (Exercicio4_2.matchDiagonalTopRight(boardGame, wordToFind, beginRow, beginColumn)) {
+                return true;
+            }
+        }
+
+        // Diagonal Bottom Left to Top Right
+        if (beginRow > endRow && beginColumn < endColumn) {
+            if (Exercicio4_2.matchDiagonalBottomLeft(boardGame, wordToFind, beginRow, beginColumn)) {
+                return true;
+            }
         }
         return false;
     }
--- src/test/java/org/example/Exercicio4/Exercicio4_3Test.java
+++ src/test/java/org/example/Exercicio4/Exercicio4_3Test.java
@@ -20,12 +20,12 @@ class Exercicio4_3Test {
 
     @ParameterizedTest
     @MethodSource("valid_tests_for_every_direction")
-    void valid_tests_for_every_direction(char[][] boardGame, ArrayList<Character> wordToFind,int rowBeginWord,
-                                         int columnBeginWord, int rowEndWord, int columnEndWord, boolean expected) {
+    void valid_tests_for_every_direction(char[][] boardGame, ArrayList<Character> wordToFind,int beginRow,
+                                         int beginColumn, int endRow, int endColumn, boolean expected) {
         // arrange
 
         // act
-        boolean result = Exercicio4_3.findWordProvided(boardGame, wordToFind, rowBeginWord, columnBeginWord, rowEndWord, columnEndWord);
+        boolean result = Exercicio4_3.findWordProvided(boardGame, wordToFind, beginRow, beginColumn, endRow, endColumn);
 
         // assert
         assertEquals(expected, result);
@@ -36,15 +36,101 @@ class Exercicio4_3Test {
                 Arguments.of((Object) new char[][]
                         {
                                 {'a', 'b', 'c'},
-                                {'d', 'e', 'f'},
-                                {'g', 'h', 'i'}
+                                {'x', 'x', 'x'},
+                                {'x', 'x', 'x'}
                         }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')), 0, 0, 0, 2, true),
                 Arguments.of((Object) new char[][]
                         {
+                                {'x', 'x', 'x'},
                                 {'a', 'b', 'c'},
-                                {'d', 'e', 'f'},
-                                {'g', 'h', 'i'}
-                        }, new ArrayList<Character>(Arrays.asList('a', 'd', 'c')), 0, 1, 0, 0, false)
+                                {'x', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')), 2, 0, 2, 2, false),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'x', 'x', 'x'},
+                                {'c', 'b', 'a'},
+                                {'x', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')), 1, 2, 1, 0, true),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'x', 'x', 'x'},
+                                {'a', 'b', 'c'},
+                                {'x', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')), 1, 2, 1, 0, false),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'a', 'x', 'x'},
+                                {'b', 'x', 'x'},
+                                {'c', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')), 0, 0, 2, 0, true),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'a', 'x', 'x'},
+                                {'b', 'x', 'x'},
+                                {'c', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),1, 1, 2, 1,false),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'c', 'x', 'x'},
+                                {'b', 'x', 'x'},
+                                {'a', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),2, 0, 0, 0, true),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'c', 'x', 'x'},
+                                {'b', 'x', 'x'},
+                                {'a', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),2, 1, 0, 1, false),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'a', 'x', 'x'},
+                                {'x', 'b', 'x'},
+                                {'x', 'x', 'c'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),0, 0, 2, 2, true),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'a', 'x', 'x'},
+                                {'x', 'b', 'x'},
+                                {'x', 'x', 'c'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),0, 1, 1, 2, false),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'c', 'x', 'x'},
+                                {'x', 'b', 'x'},
+                                {'x', 'x', 'a'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),2, 2, 0, 0, true),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'c', 'x', 'x'},
+                                {'x', 'b', 'x'},
+                                {'x', 'x', 'a'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),2, 1, 1, 0, false),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'x', 'x', 'a'},
+                                {'x', 'b', 'x'},
+                                {'c', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),0, 2, 2, 0, true),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'x', 'x', 'a'},
+                                {'x', 'b', 'x'},
+                                {'c', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),0, 1, 1, 0, false),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'x', 'x', 'c'},
+                                {'x', 'b', 'x'},
+                                {'a', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),2, 0, 0, 2, true),
+                Arguments.of((Object) new char[][]
+                        {
+                                {'x', 'x', 'c'},
+                                {'x', 'b', 'x'},
+                                {'a', 'x', 'x'}
+                        }, new ArrayList<Character>(Arrays.asList('a', 'b', 'c')),2, 1, 0, 1, false)
+
+
         );
     }
 }
