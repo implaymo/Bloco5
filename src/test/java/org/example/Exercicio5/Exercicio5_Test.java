2aa329f9b2ee1fd6bc1df69af7327907a614e39b
implaymo <goncalomoreirasilva@gmail.com>
2024-11-16 12:40:27 +0000

    Add more tests to check SudokuRules methods.

--- src/test/java/org/example/Exercicio5/Exercicio5_Test.java
+++ src/test/java/org/example/Exercicio5/Exercicio5_Test.java
@@ -442,4 +442,63 @@ class Exercicio5_Test {
                 )
         );
     }
+    @Test
+    void testIsNumberValidInRow() {
+        // arrange
+        int[][] board = {
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+        // act
+        SudokuRules rules = new SudokuRules(board);
+        // assert
+        assertFalse(rules.isNumberValidInRow(5, 0, 1)); // 5 is already in the row
+    }
+
+    @Test
+    void testIsNumberValidInColumn() {
+        // arrange
+        int[][] board = {
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+        // act
+        SudokuRules rules = new SudokuRules(board);
+        // assert
+        assertFalse(rules.isNumberValidInColumn(5, 0, 1));
+    }
+
+    @Test
+    void testIsNumberValidIn3x3Matrix() {
+        // arrange
+        int[][] board = {
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+        // act
+        SudokuRules rules = new SudokuRules(board);
+        // assert
+        assertFalse(rules.isNumberValidIn3x3Matrix(5, 0, 1));
+    }
 }
