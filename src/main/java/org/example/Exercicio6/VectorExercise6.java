695acc2b57e5ce7fe589b7232a84e23aaf5f703f
implaymo <goncalomoreirasilva@gmail.com>
2024-11-21 12:32:54 +0000

    Finish another one.

--- src/main/java/org/example/Exercicio6/VectorExercise6.java
+++ src/main/java/org/example/Exercicio6/VectorExercise6.java
@@ -1,9 +1,12 @@
 package org.example.Exercicio6;
 
+import org.example.Exercicio2.Exercicio2_1;
+import org.example.Exercicio2.Exercicio2_3;
+import org.example.Exercicio2.Exercicio2_5;
+
 public class VectorExercise6 {
 
     private int[] array;
-    private int value;
 
     public VectorExercise6() {
         this.array = new int[]{};
@@ -13,6 +16,13 @@ public class VectorExercise6 {
         this.array = array;
     }
 
+
+    public boolean isArrayEmpty() {
+        if (array.length == 0) {
+            return true;
+        }
+        return false;
+    }
 
     public int[] addValue(int value) {
         int[] newArray = new int[array.length + 1];
@@ -37,5 +47,91 @@ public class VectorExercise6 {
         return array[index];
     }
 
-    
+
+    public int getTotalElements() {
+        return Exercicio2_5.getTotalElementsInArray(array);
+    }
+
+    public int getHighestNumber(){
+        int highestNumber = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > highestNumber) {
+                highestNumber = array[i];
+            }
+        }
+        return highestNumber;
+    }
+
+    public int getLowestNumber() {
+        int lowestNumber = getHighestNumber();
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < lowestNumber) {
+                lowestNumber = array[i];
+            }
+        }
+        return lowestNumber;
+    }
+
+
+    public double getAverage() {
+        if (isArrayEmpty()) {
+            return -1;
+        }
+        double average;
+        int count = getTotalElements();
+        int sum = Exercicio2_3.sumLineNumbers(array);
+        average = (double) sum / count;
+        return average;
+    }
+
+    public double getAverageEvenNumbers(){
+        if (isArrayEmpty()) {
+            return -1;
+        }
+        double average;
+        int countEvenNumbers = 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                sum+= array[i];
+                countEvenNumbers++;
+            }
+        }
+        average = (double) sum / countEvenNumbers;
+        return average;
+    }
+
+    public double getAverageOddNumbers(){
+        if (isArrayEmpty()) {
+            return -1;
+        }
+        double average;
+        int countEvenNumbers = 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                sum+= array[i];
+                countEvenNumbers++;
+            }
+        }
+        average = (double) sum / countEvenNumbers;
+        return average;
+    }
+
+    public double getAverageOfMultiplesOfGivenNumber(int number) {
+        if (isArrayEmpty()) {
+            return -1;
+        }
+        double average;
+        int countMultipleNumbers = 0;
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % number == 0) {
+                sum+= array[i];
+                countMultipleNumbers++;
+            }
+        }
+        average = (double) sum / countMultipleNumbers;
+        return average;
+    }
 }
--- src/test/java/org/example/Exercicio6/VectorExercise6Test.java
+++ src/test/java/org/example/Exercicio6/VectorExercise6Test.java
@@ -77,4 +77,147 @@ class VectorExercise6Test {
                 Arguments.of(new int[]{-1,2,3}, 2, 3)
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider3")
+    void should_return_total_elements_array(int[] initialArray, int expected) {
+        //arrange
+        VectorExercise6 vectorExercise6 = new VectorExercise6(initialArray);
+        //act
+        int result = vectorExercise6.getTotalElements();
+        //assert
+        assertEquals(expected, result);
+    }
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider3() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,3}, 3),
+                Arguments.of(new int[]{}, 0),
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 5)
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider4")
+    void should_return_highest_value_in_array(int[] initialArray, int expected) {
+        //arrange
+        VectorExercise6 vectorExercise6 = new VectorExercise6(initialArray);
+        //act
+        int result = vectorExercise6.getHighestNumber();
+        //assert
+        assertEquals(expected, result);
+    }
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider4() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,3}, 3),
+                Arguments.of(new int[]{}, 0),
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider5")
+    void should_return_lowest_value_in_array(int[] initialArray, int expected) {
+        //arrange
+        VectorExercise6 vectorExercise6 = new VectorExercise6(initialArray);
+        //act
+        int result = vectorExercise6.getLowestNumber();
+        //assert
+        assertEquals(expected, result);
+    }
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider5() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,3}, 1),
+                Arguments.of(new int[]{}, 0),
+                Arguments.of(new int[]{0, 1, 2, 3, 4, 5}, 0)
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider6")
+    void should_return_average_of_elements_in_array(int[] initialArray, double expected) {
+        //arrange
+        VectorExercise6 vectorExercise6 = new VectorExercise6(initialArray);
+        //act
+        double result = vectorExercise6.getAverage();
+        //assert
+        assertEquals(expected, result, 0.01);
+    }
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider6() {
+        return Stream.of(
+                Arguments.of(new int[]{1,2,3}, 2),
+                Arguments.of(new int[]{}, -1),
+                Arguments.of(new int[]{0, 1, 2, 3, 4, 5}, 2.5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider7")
+    void should_return_average_of_even_number_elements_in_array(int[] initialArray, double expected) {
+        //arrange
+        VectorExercise6 vectorExercise6 = new VectorExercise6(initialArray);
+        //act
+        double result = vectorExercise6.getAverageEvenNumbers();
+        //assert
+        assertEquals(expected, result, 0.01);
+    }
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider7() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4}, 3.0),
+                Arguments.of(new int[]{}, -1),
+                Arguments.of(new int[]{0, 1, 2, 3, 4, 5}, 2.0)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider8")
+    void should_return_average_of_odd_number_elements_in_array(int[] initialArray, double expected) {
+        //arrange
+        VectorExercise6 vectorExercise6 = new VectorExercise6(initialArray);
+        //act
+        double result = vectorExercise6.getAverageOddNumbers();
+        //assert
+        assertEquals(expected, result, 0.01);
+    }
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider8() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4}, 2.0),
+                Arguments.of(new int[]{}, -1),
+                Arguments.of(new int[]{0, 1, 2, 3, 4, 5}, 3.0)
+        );
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("arrayProvider9")
+    void should_return_average_of_numbers_multiple_of_given_number(int[] initialArray, int number, double expected) {
+        //arrange
+        VectorExercise6 vectorExercise6 = new VectorExercise6(initialArray);
+        //act
+        double result = vectorExercise6.getAverageOfMultiplesOfGivenNumber(number);
+        //assert
+        assertEquals(expected, result, 0.01);
+    }
+
+    // This method provides test data to the parameterized test
+    private static Stream<Arguments> arrayProvider9() {
+        return Stream.of(
+                Arguments.of(new int[]{1, 2, 3, 4}, 2, 3.0),
+                Arguments.of(new int[]{}, 1, -1),
+                Arguments.of(new int[]{0, 1, 2, 3, 4, 5}, 3, 1.5)
+        );
+    }
 }
